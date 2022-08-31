c0c585ea95b804f52d995c18340e1b56eb801a2e
TarekHiisham <tarekhesham2017@gmail.com>
2022-08-31 23:58:50 +0200

    for "ADD_FL" : local var instead x

--- Linked list/project/ADD_FL.c
+++ Linked list/project/ADD_FL.c
@@ -2,19 +2,19 @@
 #include "../typedefs/type_def.h"
 #include "stdlib.h"
 
-void ADD_FL(u32_t x)
+void ADD_FL(u32_t data)
 { 
     if(head_ptr==NULL)
     {
         head_ptr = (node_t*)calloc(1,sizeof(node_t)) ;
-        head_ptr->Data = x ;
+        head_ptr->Data = data ;
         head_ptr->next_node = NULL ;
         last_ptr= &(head_ptr->next_node) ;
     }
     else
     {
         (*last_ptr)=(node_t*)calloc(1,sizeof(node_t));
-        (*last_ptr)->Data=x;
+        (*last_ptr)->Data= data;
         (*last_ptr)->next_node=NULL;
         last_ptr=&((*last_ptr)->next_node);
     }
