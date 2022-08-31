001654ceef7a691b33ad07619b1988717f2aba29
Shawky <amamasa121212@gmail.com>
2022-08-31 02:44:37 +0200

    some changes i hope it works this time ( T_T )

--- Linked list/project/Test.c
+++ Linked list/project/Test.c
@@ -4,7 +4,7 @@
 void ADD_FL(u32_t data) ;
 void Delete_FL(void) ;
 void ADD_FA(u32_t data , u8_t location);
-void Delete_FA(u8_t location);
+void Delete_FA(node_t** head, u8_t location);
 void printList(node_t *node);
 
 node_t *head_ptr=NULL ;
--- Linked list/project/delete_FA.c
+++ Linked list/project/delete_FA.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include "../typedefs/type_def.h"
 
-void Delete_FA(u8_t location);
-void Delete_FA(u8_t location)
+void Delete_FA(node_t** head, u8_t location);
+void Delete_FA(node_t** head, u8_t location)
 {
-    node_t* temp = head_ptr;
+    node_t* temp = *head;
     node_t* previous = (node_t*) malloc(sizeof(node_t));
 
-    if (head_ptr == NULL)
+    if (*head == NULL)
     {
         printf("The linked list is empty");
         return;
@@ -16,8 +16,8 @@ void Delete_FA(u8_t location)
 
     else if (temp -> next_node == NULL)
     {
-        printf("The data is deleted: %d",(head_ptr) -> Data);
-        head_ptr = NULL;
+        printf("The data is deleted: %d",(*head) -> Data);
+        *head = NULL;
     }
     
     else
@@ -30,7 +30,7 @@ void Delete_FA(u8_t location)
             temp = temp -> next_node;
         }
     
-        previous -> next_node = NULL;
+        previous -> next_node = temp -> next_node;
         printf ("The data is deleted: %d",temp -> Data);
     }
 
